54f84c7733c6a92634390b0f8ccb5c324963db89
Helen Nelson <hnelson@fandm.edu>
2021-04-27 23:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ function divide(){
 function add(a, b){
   return (a + b)
 }
+
+function subtract(a, b){
+  return (a - b)
+}
+
+function multiply(a, b){
+  return (a * b)
+}
+
+function divide(a, b){
+  return (a / b)
+}
+
+function increment(n){
+  return (n += 1)
+}
+
+function decrement(n){
+  return (n -= 1)
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
